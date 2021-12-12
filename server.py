a14a3770f22ba495354f14c742e71f2cc995893c
Renamethis <ops282@mail.ru>
2021-12-12 06:16:09 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,9 +1,23 @@
+
 import subprocess
 from random import random
 import requests
+import os
+import shutil
 while True:
-    iterations = 40 + int(random()*50)
-    response = requests.get("https://random-word-api.herokuapp.com/word?number=3").json()
-    sentence = "\"" + response[0] + " " + response[1] + " " + response[2] + "\""
-    res = subprocess.run(["/usr/bin/python", "torch_run.py", str(iterations), ])
+    try:
+        shutil.rmtree("content/steps")
+        os.mkdir("content/steps")
+    except:
+        os.mkdir("content/steps")
+    iterations = 200 + int(random()*400)
+    num_words = 2 + int(random()*5)
+    response = requests.get(("https://random-word-api.herokuapp.com/word?number=" + str(num_words))).json()
+    sentence = "\""
+    for r in response:
+        sentence += (r + " ")
+    sentence += "\""
+    res = subprocess.run(["/usr/bin/python3", "torch_run.py", str(iterations), sentence])
+    res = subprocess.check_output(["/bin/imgur", "content/movie.gif"])
+    print(res.decode("utf-8").split("page:")[0][:-1])
     ### REQUEST ON BACKEND
